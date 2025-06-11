8acb60889ba7e7f3fbdc4d0b653f7a98ebef5a39
Esteban V.M. <estebanvm90@outlook.com>
2025-06-11 16:37:58 -0300

    update app

--- src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
+++ src/main/java/com/aluracursos/literalura/LiteraluraApplication.java
@@ -1,11 +1,27 @@
 package com.aluracursos.literalura;
 
+import com.aluracursos.literalura.main.Main;
+import com.aluracursos.literalura.repositories.AuthorRepository;
+import com.aluracursos.literalura.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
+    @Autowired
+    private BookRepository bookRepository;
+    @Autowired
+    private AuthorRepository authorRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(LiteraluraApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        Main main = new Main(bookRepository, authorRepository);
+        main.showMenu();
     }
 }
